7fabbd007e2db93c4d8e1d1553f6fda911933f6a
Andcruz <Andressa_Cruz@gap.com>
2021-11-23 23:33:18 -0300

    a

--- src/main/java/com/dev/software/devsoftware/controllers/FavoriteHospitalController.java
+++ src/main/java/com/dev/software/devsoftware/controllers/FavoriteHospitalController.java
@@ -2,10 +2,9 @@ package com.dev.software.devsoftware.controllers;
 
 import com.dev.software.devsoftware.models.FavoriteHospital;
 import com.dev.software.devsoftware.models.User;
-import com.dev.software.devsoftware.models.api.Hospital;
 import com.dev.software.devsoftware.models.api.HospitalList;
-import com.dev.software.devsoftware.models.dto.HospitalsResponse;
 import com.dev.software.devsoftware.repository.FavoriteHospitalRepository;
+import com.dev.software.devsoftware.repository.UserRepository;
 import com.dev.software.devsoftware.service.HospitalsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,14 +12,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class FavoriteHospitalController {
+
+	@Autowired
+	UserRepository userRepository;
 
 	@Autowired
 	FavoriteHospitalRepository favoriteHospitalRepository;
@@ -38,10 +37,19 @@ public class FavoriteHospitalController {
 	}
 
 	@PostMapping({"favorite-hospital"})
-	public String addFavoriteHospital(@ModelAttribute(value="hospital") String hospital, RedirectAttributes redirectAttributes) {
+	public String addFavoriteHospital(@ModelAttribute(value = "hospitalName") String hospitalName, @ModelAttribute(value = "userEmail") String userEmail) {
+		User user = userRepository.findByEmail(userEmail);
+
 		// validation pode ser extraido depois
-		if(hospital != null){
-			redirectAttributes.addFlashAttribute("hospital", hospital);
+		if(hospitalName != null && userEmail != null && user != null){
+
+			FavoriteHospital favoriteHospital = new FavoriteHospital();
+			favoriteHospital.setUser(user);
+			favoriteHospital.setName(hospitalName);
+			favoriteHospital.setIdHospital(hospitalName);
+
+			favoriteHospitalRepository.save(favoriteHospital);
+
 			return "redirect:/list-favorites";
 		}
 		return "/";
